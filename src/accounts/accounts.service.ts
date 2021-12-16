1426d4a4310ed1bab937b4edf0bd08fb356e961c
Matheus <mrehbein45@gmail.com>
2021-12-16 17:38:35 -0300

    created accounts model

--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,26 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { UpdateAccountDto } from './dto/update-account.dto';
+import { Account } from './entities/account.entity';
 
 @Injectable()
 export class AccountsService {
+  constructor(@InjectModel(Account) private accountModel: typeof Account){}
+
   create(createAccountDto: CreateAccountDto) {
-    return 'This action adds a new account';
+    return this.accountModel.create(createAccountDto);
   }
 
   findAll() {
-    return `This action returns all accounts`;
+    return this.accountModel.findAll();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} account`;
+  findOne(id: string) {
+    return this.accountModel.findByPk(id);
   }
 
-  update(id: number, updateAccountDto: UpdateAccountDto) {
-    return `This action updates a #${id} account`;
+  async update(id: string, updateAccountDto: UpdateAccountDto) {
+    const account = await this.accountModel.findByPk(id)
+    return account.update(updateAccountDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} account`;
+  async remove(id: string) {
+    const account = await this.accountModel.findByPk(id)
+    return account.destroy();
   }
 }
