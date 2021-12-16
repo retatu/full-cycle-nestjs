dd5ed5b8a64d01a7b15bbded33e800fce0a690df
Matheus <mrehbein45@gmail.com>
2021-12-16 17:57:52 -0300

    created service to set the token from header

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -3,10 +3,11 @@ import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Account } from './entities/account.entity';
+import { AccountStorageService } from './account-storage/account-storage.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([Account])],
   controllers: [AccountsController],
-  providers: [AccountsService]
+  providers: [AccountsService, AccountStorageService]
 })
 export class AccountsModule {}
